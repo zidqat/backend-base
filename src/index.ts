49d257c375272b733a1308df4c7d586527bf4c1e
Carlos Marin Diaz <carlosmarind@gmail.com>
2024-07-24 17:05:20 -0400

    mi primer commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import { configuration } from "./config.js";
 import app from "./server.js";
 
 const puerto = 3000;
 
 app.listen(puerto, () => {
-  console.log("servidor levantado en puerto: ", puerto);
+  console.log(`El usuario ${configuration.username} ha levantado la aplicacion en el puerto`, puerto);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
-
+import { configuration } from "./config.js";
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hola mundo");
+  res.send(`Hola mundo al usuario ${configuration.username}`);
 });
 
 export default app;
